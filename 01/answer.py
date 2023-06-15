00fa88e7cc0bbf06cd11637577aed4700f1cc341
Yubraj Bhoi <yubrajbhoi999@gmail.com>
2023-06-15 16:03:59 +0530

    Initial commit
    Add .gitignore
    Add README file
    Add licence file
    Add solution problem 01

---
+++ 01/answer.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pyopencl as cl
+import pyopencl.array
+
+
+KERNEL = """
+__kernel void filter(__global int *x) {
+    int gid = get_global_id(0);
+    int i = x[gid];
+    if (i % 3 != 0 && i % 5 != 0) {
+        x[gid] = 0;
+    }
+}
+"""
+
+
+def main():
+    dtype = "int32"
+    n = 1000
+
+    ctx = cl.create_some_context()
+    queue = cl.CommandQueue(ctx)
+
+    x = np.array(list(range(1, n)), dtype=dtype)
+    clx = cl.array.Array(queue, x.shape, x.dtype)
+    clx.set(x)
+
+    prg = cl.Program(
+        ctx,
+        KERNEL,
+    ).build()
+
+    prg.filter(queue, clx.shape, None, clx.data)
+    queue.finish()
+    print(sum(clx.get()))
+
+
+if __name__ == "__main__":
+    main()
---
+++ README.md
@@ -0,0 +1,9 @@
+# Project Euler in OpenCL
+
+Project Euler solutions in OpenCL. Most of them are not optimised for speed. It is for learning OpenCL only.
+
+# Dependencies
+
+ - PyOpenCL
+
+ - Numpy
